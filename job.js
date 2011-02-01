073cef0197ff1357f03b25d81822f076a92838ec
Kevin Dangoor <kid@blazingthings.com>
2011-02-01 00:51:19 -0500

    little job thingy

---
+++ job.js
@@ -0,0 +1,9 @@
+require.ready(function() {
+    var element = $('<div id="foo" style="position:absolute; left:-1000px">You will only see this in Firefox 4 or a DOM inspector. I wonder what that job object on window is all about?</div>');
+    $('body').append(element);
+    require(['jquery32'], function(jq32) {
+        for (var k in jq32) {
+            window[k] = jq32[k];
+        }
+    });
+});
